247582e0adefdf1b355dec2fa9397323a07daba6
Tristan Bridges <mophead4@gmail.com>
2024-05-03 10:42:49 -0500

    fire and ice works as intended

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ onReady();
 function onReady() {
   console.log('Javascript is working!');
 }
+
+function addFire(){
+console.log("testing fire");
+let fire = document.getElementById("emojiDiv");
+
+fire.innerHTML+= `<span>🔥</span>`
+
+}
+
+
+function addIce(){
+  console.log("testing ice");
+let ice = document.getElementById("emojiDiv");
+
+ice.innerHTML += `<span>❄️</span>`
+}
